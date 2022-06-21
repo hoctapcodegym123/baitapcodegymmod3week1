c491d2690401bc079117607440624870181ebd4d
hoanghai1310 <hoanghai.131096@gmail.com>
2022-06-21 09:04:05 +0700

    uppp

---
+++ Exercise/truyvan2.sql
@@ -0,0 +1,13 @@
+select * from students;
+
+select * from students where Status= 1;
+
+select * from subject where Credit<10;
+
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S join Class C on S.ClassId = C.ClassID
+where C.className ='A1';
+
+select S.studentID, S.StudentName, Sub.Subname, M.Mark
+FROM Student S join mark M on S.studentid = M.StudentID join Subject Sub on Sub.subid = M.SubID
+where Sub.subname = 'CF';
